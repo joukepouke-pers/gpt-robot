3671a9b0d0d6fd1097acf9afc557bb908f1b6dc6
jouke <jouke@tayami.net>
2023-07-06 15:12:18 +0200

    it works now

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 import api_call
-print(api_call.start(input("you: ")))
+print(api_call.start_conversation(input("you: ")))
 
 while True:
     api_call.userinput(input("you: "))
--- api_call.py
+++ api_call.py
@@ -1,8 +1,8 @@
 import openai
 import json
-import functions
-api_key = open("openai_api_key", "r")
-start_conversation = [
+import gpt_functions
+openai.api_key_path = "openai_api_key"
+default_conversation = [
         {
             "role" : "system",
             "content" : 'you are a language model made by openai wich is intergrated into a voice assistant. give short responses. only call functions provided to you.'
@@ -16,11 +16,11 @@ functions = [
             "type" : "object",
             "properties" : {
                 "direction" : {
-                    "type" : "int",
-                    "description" : "1 to move forward, 0 to move backward"
+                    "type" : "integer",
+                    "description" : "either forward(1) or backward(0)"
                     },
                 "amount" : {
-                    "type" : "int",
+                    "type" : "integer",
                     "description" : "amount of centimeters to move"
                     },
                 },
@@ -30,12 +30,15 @@ functions = [
 ]
 def start_conversation(message):
     global conversation
-    conversation = start_conversation
+    conversation = default_conversation
     return continue_conversation({ "role" : "user", "content" : message})
     
 def continue_conversation(message_json):
     global conversation
-    conversation.pop(1)
+    try:
+        conversation.pop(1)
+    except:
+        pass
     conversation.append(message_json)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0613",
@@ -46,7 +49,7 @@ def continue_conversation(message_json):
     response_message = response["choices"][0]["message"]
     if response_message.get("function_call"):
         available_functions = {
-                "move" : functions.move
+                "move" : gpt_functions.move
                 }
         function_name = response_message["function_call"]["name"]
         fuction_to_call = available_functions[function_name]
