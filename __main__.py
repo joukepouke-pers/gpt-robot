5559e6a100f8fd111113f2c37a27e96b0b89d533
jouke <jouke@tayami.net>
2023-07-12 15:14:25 +0200

    added something i dont remember just a casual commit

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 import api_call
-print(api_call.start_conversation(input("you: ")))
+print(api_call.start_conversation(input("you: "))["content"])
 
 while True:
-    print(api_call.userinput(input("you: ")))
+    print(api_call.userinput(input("you: "))["content"])
 
